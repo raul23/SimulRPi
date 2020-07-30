447963aee803da1371bf6a1775d7404f24734e56
Raul <rchfe23@gmail.com>
2020-07-29 22:46:35 -0400

    Update description

--- SimulRPi/GPIO.py
+++ SimulRPi/GPIO.py
@@ -65,8 +65,7 @@ class Pin:
 
     See Also
     --------
-    :class:`PinDB` : Class for storing and accessing :class:`Pin`\s.
-
+    :class:`PinDB` : Class for storing and accessing :class:`Pin`.
 
     """
     def __init__(self, channel, mode, key=None, pull_up_down=None, initial=None):
