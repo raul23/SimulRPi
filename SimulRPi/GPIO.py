d7a4408d31c936dbe9a6cc6c7fe09d5ec5ff360f
Raul <rchfe23@gmail.com>
2020-07-29 23:44:37 -0400

    Update description

--- SimulRPi/GPIO.py
+++ SimulRPi/GPIO.py
@@ -9,9 +9,7 @@ It simulates these I/O devices connected to a Raspberry Pi:
 When a LED is turn on, it is shown as a small red circle on the terminal. The
 package `pynput`_ is used to monitor the keyboard for any key pressed.
 
-**Example: terminal output**
-
-.. code:: bash
+**Example: terminal output**::
 
     o [11]   o [9]   o [10]
 
