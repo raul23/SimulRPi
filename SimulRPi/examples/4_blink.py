f261335e0212078491c54fcf4a281e3d4e8468ab
Raul <rchfe23@gmail.com>
2020-08-05 22:47:11 -0400

    4_blink.py: Update description

--- SimulRPi/examples/4_blink.py
+++ SimulRPi/examples/4_blink.py
@@ -1,11 +1,27 @@
+#!/usr/script/env python
 """Example 4: blink a LED for 4 seconds
 
-Blink a LED on channel 20 for 4 seconds (or until you press :obj:`ctrl` +
-:obj:`c`)
+This script allows you to blink a LED on channel 20 for 4 seconds (or until you
+press :obj:`ctrl` + :obj:`c`).
+
+The script can be run on a Raspberry Pi (RPi) or on your computer in which case
+it will make use of the mocK library `SimulRPi
+<https://github.com/raul23/SimulRPi>`_.
+
+Usage
+-----
+
+Run the script on the RPi::
+
+    $ 4_blink_py
+
+Run the script on your computer (simulation flag)::
+
+    $ 4_blink_py -s
 
 """
 import time
-import SimulRPi.GPIO as GPIO
+GPIO = None
 
 
 def blink_led(channel):
@@ -38,4 +54,9 @@ def blink_led(channel):
 
 
 if __name__ == '__main__':
+    if args.simulation:
+        import SimulRPi.GPIO as GPIO
+        print("Simulation mode enabled")
+    else:
+        import RPi.GPIO as GPIO
     blink_led(20)
