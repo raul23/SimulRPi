80fca3dc5d94c4f3c2caf9fc2381d8a8d28be2f1
Raul <rchfe23@gmail.com>
2020-08-05 23:44:04 -0400

    utils: add blink_led()

--- SimulRPi/utils.py
+++ SimulRPi/utils.py
@@ -1,7 +1,28 @@
+import time
+
 try:
     import RPi.GPIO as GPIO
 except ImportError:
     import SimulRPi.GPIO as GPIO
+
+
+def blink_led(channel, time_on, time_off):
+    """Blink one LED.
+
+    A LED on the given `channel` will be turned on and off for `time_one` and
+    `time_off` seconds, respectively.
+
+    Parameters
+    ----------
+    channel
+    time_on
+    time_off
+
+    """
+    turn_on_led(channel)
+    time.sleep(time_on)
+    turn_off_led(channel)
+    time.sleep(time_off)
 
 
 def turn_off_led(channel):
