434c2fda96ba6606f5ae08affc043a68b35218f6
Raul <rchfe23@gmail.com>
2020-08-09 16:30:27 -0400

    mapping: remove default keymap which is now in its own file

--- SimulRPi/mapping.py
+++ SimulRPi/mapping.py
@@ -12,7 +12,8 @@ In early RPi models, there are 17 GPIO channels and in late RPi models, there
 are 28 GPIO channels.
 
 By default, 28 GPIO channels (from 0 to 27) are mapped to alphanumeric and
-special keys.
+special keys. See the :ref:`content of the default keymap
+<content-default-keymap-label>`.
 
 Here is the full list of special keys you can use with info about some of them
 (taken from `pynput reference`_):
@@ -80,44 +81,9 @@ References
 .. _pynput reference: https://pynput.readthedocs.io/en/latest/keyboard.html#pynput.keyboard.Key
 
 """
-# from SimulRPi.utils import convert_keys_to_int
+from SimulRPi.default_keymap import default_key_to_channel_map
 
 
-# TODO: specify in docs that key must be a string even if key is a numeric key
-# [start-section]
-default_key_to_channel_map = {
-    "0": 0,  # sudo on mac
-    "1": 1,  # sudo on mac
-    "2": 2,  # sudo on mac
-    "3": 3,  # sudo on mac
-    "4": 4,  # sudo on mac
-    "5": 5,  # sudo on mac
-    "6": 6,  # sudo on mac
-    "7": 7,  # sudo on mac
-    "8": 8,  # sudo on mac
-    "9": 9,  # sudo on mac
-    "q": 10,  # sudo on mac
-    "alt": 11,  # left alt on mac
-    "alt_l": 12,  # not recognized on mac
-    "alt_r": 13,
-    "alt_gr": 14,
-    "cmd": 15,  # left cmd on mac
-    "cmd_l": 16,  # not recognized on mac
-    "cmd_r": 17,
-    "ctrl": 18,  # left ctrl on mac
-    "ctrl_l": 19,  # not recognized on mac
-    "ctrl_r": 20,
-    "media_play_pause": 21,
-    "media_volume_down": 22,
-    "media_volume_mute": 23,
-    "media_volume_up": 24,
-    "shift": 25,  # left shift on mac
-    "shift_l": 26,  # not recognized on mac
-    "shift_r": 27,
-}
-# [end-section]
-
-# TODO: necessary conversion?
-# default_key_to_channel_map = convert_keys_to_int(default_key_to_channel_map)
+# Reverse keymap: channel to key
 default_channel_to_key_map = {v: k for k, v in
                               default_key_to_channel_map.items()}
