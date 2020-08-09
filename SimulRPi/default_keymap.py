8edb47ebdfe45b1c070121144f5e4bfa9805e8b2
Raul <rchfe23@gmail.com>
2020-08-09 16:12:54 -0400

    Add default keymap in a separate file

---
+++ SimulRPi/default_keymap.py
@@ -0,0 +1,30 @@
+default_key_to_channel_map = {
+    "0": 0,  # sudo on mac
+    "1": 1,  # sudo on mac
+    "2": 2,  # sudo on mac
+    "3": 3,  # sudo on mac
+    "4": 4,  # sudo on mac
+    "5": 5,  # sudo on mac
+    "6": 6,  # sudo on mac
+    "7": 7,  # sudo on mac
+    "8": 8,  # sudo on mac
+    "9": 9,  # sudo on mac
+    "q": 10,  # sudo on mac
+    "alt": 11,  # left alt on mac
+    "alt_l": 12,  # not recognized on mac
+    "alt_r": 13,
+    "alt_gr": 14,
+    "cmd": 15,  # left cmd on mac
+    "cmd_l": 16,  # not recognized on mac
+    "cmd_r": 17,
+    "ctrl": 18,  # left ctrl on mac
+    "ctrl_l": 19,  # not recognized on mac
+    "ctrl_r": 20,
+    "media_play_pause": 21,
+    "media_volume_down": 22,
+    "media_volume_mute": 23,
+    "media_volume_up": 24,
+    "shift": 25,  # left shift on mac
+    "shift_l": 26,  # not recognized on mac
+    "shift_r": 27,
+}
