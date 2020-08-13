4503373c384dcd66dfa49d63e88396caeddb06f6
Raul <rchfe23@gmail.com>
2020-08-13 19:26:47 -0400

    setup: add testing argument to script to choose correct SimulRPi version (test or real)

--- setup.py
+++ setup.py
@@ -5,10 +5,19 @@ The PyPi project name is `SimulRPi.GPIO` and package name is `SimulRPi`.
 """
 
 import os
+import sys
 from setuptools import find_packages, setup
 
-from SimulRPi import __version__
+from SimulRPi import __version__, __test_version__
 
+
+# Choose the correct version based on script's arg
+if sys.argv[1] == "testing":
+    VERSION = __test_version__
+    # Remove "testing" from args so setup doesn't process "testing" as a cmd
+    sys.argv.remove("testing")
+else:
+    VERSION = __version__
 
 # Directory of this file
 dirpath = os.path.abspath(os.path.dirname(__file__))
@@ -22,7 +31,7 @@ with open(os.path.join(dirpath, "requirements.txt")) as f:
     REQUIREMENTS = f.read().split()
 
 setup(name='SimulRPi',
-      version=__version__,
+      version=VERSION,
       description='A library that partly fakes RPi.GPIO and simulates some I/O '
                   'devices on a Raspberry Pi.',
       long_description=README,
@@ -50,4 +59,9 @@ setup(name='SimulRPi',
       entry_points={
           'console_scripts': ['run_examples=SimulRPi.run_examples:main']
       },
+      project_urls={  # Optional
+          'Bug Reports': 'https://github.com/raul23/SimulRPi/issues',
+          'Documentation': 'https://simulrpi.readthedocs.io/en/latest/index.html',
+          'Source': 'https://github.com/raul23/SimulRPi',
+        },
       zip_safe=False)
