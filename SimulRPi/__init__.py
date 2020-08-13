d534851aab7bfbd078128f44d081074cd67a161d
Raul <rchfe23@gmail.com>
2020-08-13 19:27:24 -0400

    Update SimulRPi versions

--- SimulRPi/__init__.py
+++ SimulRPi/__init__.py
@@ -1,3 +1,4 @@
+# For debugging purposes
+__test_version__ = "0.3.0.post2"
 # Version of package
-__version__ = "0.1.0a0"  # real pypi
-# __version__ = "0.3.0post0"  # test pypi
+__version__ = "0.1.0a0"  # real
