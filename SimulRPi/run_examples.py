a92655056965c95c599824becf685bd06651b691
Raul <rchfe23@gmail.com>
2020-09-12 18:46:03 -0400

    Update doc

--- SimulRPi/run_examples.py
+++ SimulRPi/run_examples.py
@@ -45,10 +45,10 @@ Run the code for example 1 on your **computer** using :mod:`SimulRPi.GPIO`::
 .. _RPi.GPIO: https://pypi.org/project/RPi.GPIO/
 .. _SimulRPi: https://pypi.org/project/SimulRPi
 .. _SimulRPi.GPIO: https://pypi.org/project/SimulRPi
-.. _script's usage: #usage
 
 .. internal links
 .. _installed: README_docs.html#installation-instructions
+.. _script's usage: #usage
 
 .. TODO: find if we can load the list of options from a separate file
 
